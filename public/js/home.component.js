53e00dedc608f3e3d93a9906186c51a2f7c013b0
Cassandra Torske <cassandra.torske@gmail.com>
2017-05-12 14:18:36 -0600

    fun log

--- public/js/home.component.js
+++ public/js/home.component.js
@@ -6,6 +6,11 @@ function HomeController($http, $stateParams, $state) {
   const vm = this
 
   vm.$onInit = function() {
+    console.rand("It's like everyone tells a story about themselves inside their own head.")
+    console.rand("Always. All the time.");
+    console.rand("That story makes you what you are.")
+    console.rand("We build ourselves out of that story.")
+    console.rand("(Patrick Rothfuss)")
   }
 
 }
